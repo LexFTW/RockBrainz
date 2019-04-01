4fdb0cf50c4b69059db00b790b05b266f3ce8314
LexFTW <lexmengual@gmail.com>
2019-04-01 20:28:44 +0200

    01/04/2019 20:28

--- RockBrainzProject/www/js/query.js
+++ RockBrainzProject/www/js/query.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  $( "#btnSearchSong" ).click(function() {
+  $( "#artists" ).click(function() {
     $("thead").empty();
     $("tbody").empty();
     $.ajax({
@@ -12,6 +12,28 @@ $(document).ready(function(){
         console.log(msg.artists[item]);
         console.log(msg.artists[item]["name"]);
         $("tbody").append("<tr><td>" + msg.artists[item]["name"] + "</td><td>" +msg.artists[item]["disambiguation"]+ "</tr>")
+      };
+    }).fail(function () {
+      alert("ERROR");
+    });
+  });
+
+  $( "#recordings" ).click(function() {
+    $("thead").empty();
+    $("tbody").empty();
+    $.ajax({
+      method: "GET",
+      url: "https://musicbrainz.org/ws/2/recording?query=" + $("#group").val(),
+      dataType: "json",   // necessitem això pq ens retorni un objecte JSON
+    }).done(function (msg) {
+      $("thead").append("<tr><th scope='col'>Title</th><th scope='col'>Arists</th></tr>");
+      for(var item in msg.recordings) {
+        var artists = "";
+        for(var i in msg.recordings[item]["artist-credit"]){
+          var artist = msg.recordings[item]["artist-credit"][i];
+          artists = artists + artist["artist"]["name"] + ", ";
+        }
+        $("tbody").append("<tr><td>" + msg.recordings[item]["title"] + "</td><td>" +artists+ "</tr>")
       };
     }).fail(function () {
       alert("ERROR");
