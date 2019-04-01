c4a38cc7ee83e043dcb8c536b0c746fe826b4370
LexFTW <lexmengual@gmail.com>
2019-04-01 19:53:34 +0200

    01/04/2019 19:53

--- RockBrainzProject/www/js/query.js
+++ RockBrainzProject/www/js/query.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
   $( "#btnSearchSong" ).click(function() {
-    console.log($("#group").text());
+    $("thead").empty();
+    $("tbody").empty();
     $.ajax({
       method: "GET",
       url: "https://musicbrainz.org/ws/2/artist?query=" + $("#group").val(),
       dataType: "json",   // necessitem això pq ens retorni un objecte JSON
     }).done(function (msg) {
+      $("thead").append("<tr><th scope='col'>Nombre</th><th scope='col'>Disambiguation</th></tr>");
       for(var item in msg.artists) {
         console.log(msg.artists[item]);
         console.log(msg.artists[item]["name"]);
-        $("tbody").append("<tr><td>" + msg.artists[item]["name"] + "</td></tr>")
+        $("tbody").append("<tr><td>" + msg.artists[item]["name"] + "</td><td>" +msg.artists[item]["disambiguation"]+ "</tr>")
       };
     }).fail(function () {
       alert("ERROR");
